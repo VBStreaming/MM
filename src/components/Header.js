3beb263497b03224d671994a0db9f270763fc415
huni0i <huni0i@naver.com>
2026-06-16 10:29:53 +0900

    feat: add bracket navigation header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
-        <header className="App-header">
-            <h1>Welcome to my React App</h1>
+        <header className="app-header">
+            <Link className="logo" to="/bracket">BracketMaster</Link>
+            <nav>
+                <Link to="/bracket">대진표</Link>
+                <Link to="/login">로그인</Link>
+            </nav>
         </header>
     )
 }
